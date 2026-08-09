63512bd72cdec26b748bae27d65d60e0c9396a5a
Rinat <tiran678@icloud.com>
2026-08-09 19:04:43 +0300

    feat: add framework-free render observer

---
+++ packages/render-observer/src/index.ts
@@ -0,0 +1,11 @@
+export { startFrameSample } from './frame-sampler';
+export { createRenderObserver } from './observer';
+export type {
+  FrameSample,
+  FrameSampleOptions,
+  FrameScheduler,
+  RenderContext,
+  RenderContextValue,
+  RenderObserver,
+  RenderSnapshot,
+} from './types';
